a9efbea6804a73f293adc18b26ad359b714d045e
Lucas Lima T <lukaslymauchiha@gmail.com>
2020-02-24 12:17:23 -0300

    Initial commit

---
+++ app/src/main/java/multi/version/lln/MainActivity.java
@@ -0,0 +1,13 @@
+package multi.version.lln;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
